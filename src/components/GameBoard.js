f5c7069db5616d8b1c6a49c3ad08efa71779581a
AmandaPuskyte <62665663+AmandaPuskyte@users.noreply.github.com>
2024-05-06 18:46:56 +0300

    Initial commit

---
+++ src/components/GameBoard.js
@@ -0,0 +1,22 @@
+export default function GameBoard({ onSelecetSquare, broad }) {
+  return (
+    <ol id="game-board">
+      {broad.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelecetSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
